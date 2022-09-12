d010b20e3fa6550636ba367eb56e100c25666f10
Daniël de Kok <me@danieldk.eu>
2022-09-12 10:47:12 +0200

    Test tranformer pipe against HF transformers

--- spacy_experimental/transformers/tests/test_pipe.py
+++ spacy_experimental/transformers/tests/test_pipe.py
@@ -1,7 +1,16 @@
 from numpy.testing import assert_array_equal
+import pytest
+import spacy
 from spacy import Config, util
 from spacy.training import Example
+from thinc.api import NumpyOps
 from thinc.backends import get_current_ops
+import torch
+
+from spacy_experimental.transformers.models.transformer_model import build_xlmr_transformer_model_v1
+from spacy_experimental.transformers.models.with_strided_spans import build_with_strided_spans_v1
+from spacy_experimental.transformers.pipe import make_transformer
+from spacy_experimental.transformers._compat import has_hf_transformers, transformers
 
 
 cfg_string = """
@@ -66,3 +75,52 @@ def test_tagger():
     docs = list(nlp.pipe(["Eat blue ham", "I like green eggs"]))
     assert [t.tag_ for t in docs[0]] == ["V", "J", "N"]
     assert [t.tag_ for t in docs[1]] == ["N", "V", "J", "N"]
+
+
+def _hf_tokenize_per_token(tokenizer, docs):
+    hf_encoding = [tokenizer([token.text for token in doc]) for doc in docs]
+    ids = []
+    lens = []
+    for i in range(len(hf_encoding)):
+        doc_ids = [id for e in hf_encoding[i]["input_ids"] for id in e[1:-1]]
+        ids.append([tokenizer.bos_token_id] + doc_ids + [tokenizer.eos_token_id])
+        lens.append(len(ids[-1]))
+
+    torch_ids = torch.full(
+        (len(ids), max(lens)), tokenizer.pad_token_id, dtype=torch.int
+    )
+    for i in range(len(ids)):
+        torch_ids[i][: len(ids[i])] = torch.tensor(ids[i])
+
+    attention_mask = torch_ids.ne(tokenizer.pad_token_id)
+
+    return torch_ids, attention_mask, lens
+
+
+@pytest.mark.slow
+@pytest.mark.skipif(not has_hf_transformers, reason="requires 🤗 transformers")
+def test_transformer_pipe_against_hf():
+    nlp = spacy.blank("en")
+    model = build_xlmr_transformer_model_v1(
+        with_spans=build_with_strided_spans_v1(), hf_model_name="xlm-roberta-base"
+    )
+    pipe = make_transformer(nlp, "transformer", model)
+
+    hf_tokenizer = transformers.AutoTokenizer.from_pretrained("xlm-roberta-base")
+    hf_model = transformers.AutoModel.from_pretrained("xlm-roberta-base")
+
+    docs = [
+        nlp.make_doc("I saw a girl with a telescope."),
+        nlp.make_doc("Today we will eat poké bowl."),
+    ]
+
+    hf_ids, attention_mask, lens = _hf_tokenize_per_token(hf_tokenizer, docs)
+    hf_encoding = hf_model(hf_ids, attention_mask=attention_mask)
+    docs = list(pipe.pipe(docs))
+
+    for doc, hf_doc_encoding, encoding_len in zip(
+        docs, hf_encoding.last_hidden_state, lens
+    ):
+        torch.testing.assert_allclose(
+            hf_doc_encoding[:encoding_len][1:-1], doc._.trf_data.dataXd
+        )

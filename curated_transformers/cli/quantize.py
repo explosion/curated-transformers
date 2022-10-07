c4eb68e6f8f58594a28df4411a8f60ca9f5a8a42
Daniël de Kok <me@danieldk.eu>
2022-10-07 13:21:48 +0200

    quantize: add `max-mse-loss` option
    This option excludes parameters from quantization for which the MSE would be
    larger than the given value.

--- curated_transformers/cli/quantize.py
+++ curated_transformers/cli/quantize.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from io import BytesIO
 from pathlib import Path
 import spacy
@@ -5,7 +6,8 @@ from spacy import Language
 from spacy.cli import app
 from thinc.api import Model, PyTorchShim
 import torch
-from torch.nn import Embedding, Linear, Module
+import torch.nn.quantized as nnq
+from torch.nn import Embedding, Linear, Module, MSELoss
 from torch.quantization import qconfig
 from typer import Argument as Arg, Option
 
@@ -13,10 +15,19 @@ from ..models.torchscript_wrapper import to_torchscript_wrapper
 from ..pipe import Transformer
 
 
+MODULE_QUANTIZERS = {
+    Embedding: qconfig.float_qparams_weight_only_qconfig,
+    Linear: qconfig.default_dynamic_qconfig,
+}
+
+
 @app.command("quantize-transformer")
 def quantize_cli(
     model_path: Path = Arg(..., help="Model to quantize", exists=True, allow_dash=True),
     output_path: Path = Arg(..., help="Output directory to store quantized model in"),
+    max_mse_loss: Optional[float] = Option(
+        None, "--max-mse-loss", help="Maximum MSE loss of quantized parameters"
+    ),
     skip_embeds: bool = Option(
         False, "--skip-embeds", help="Do not quantize embeddings"
     ),
@@ -28,7 +39,9 @@ def quantize_cli(
     Quantize a curated-transformers model.
     """
     nlp = spacy.load(model_path)
-    nlp_quantize_dynamic(nlp, skip_embeds=skip_embeds, skip_linear=skip_linear)
+    nlp_quantize_dynamic(
+        nlp, max_mse_loss=max_mse_loss, skip_embeds=skip_embeds, skip_linear=skip_linear
+    )
     nlp.to_disk(output_path)
 
 
@@ -39,7 +52,11 @@ def size_of_model(model):
 
 
 def nlp_quantize_dynamic(
-    nlp: Language, *, skip_embeds: bool = False, skip_linear: bool = False
+    nlp: Language,
+    *,
+    max_mse_loss: Optional[float] = None,
+    skip_embeds: bool = False,
+    skip_linear: bool = False,
 ):
     for pipe_name, pipe in nlp.components:
         # We only quantize curated transformers, other models may not
@@ -53,7 +70,12 @@ def nlp_quantize_dynamic(
             continue
 
         before_size = size_of_model(pytorch_model(model))
-        quantize_dynamic(model, skip_embeds=skip_embeds, skip_linear=skip_linear)
+        quantize_dynamic(
+            model,
+            max_mse_loss=max_mse_loss,
+            skip_embeds=skip_embeds,
+            skip_linear=skip_linear,
+        )
         after_size = size_of_model(pytorch_model(model))
         print(
             f"Quantized model in pipe '{pipe_name}' ({before_size/2**20:.1f} MiB -> {after_size/2**20:.1f} MiB)"
@@ -64,7 +86,10 @@ def nlp_quantize_dynamic(
 
 
 def quantize_dynamic(
-    model: Model, skip_embeds: bool = False, skip_linear: bool = False
+    model: Model,
+    max_mse_loss: Optional[float] = None,
+    skip_embeds: bool = False,
+    skip_linear: bool = False,
 ):
     assert model.name == "pytorch"
     assert isinstance(model.shims[0], PyTorchShim)
@@ -72,19 +97,52 @@ def quantize_dynamic(
     pytorch_model = model.shims[0]._model
     quantize_types = {}
 
-    if not skip_linear:
-        quantize_types[Linear] = qconfig.default_dynamic_qconfig
-
     if not skip_embeds:
-        quantize_types[Embedding] = qconfig.float_qparams_weight_only_qconfig
+        quantize_types[Embedding] = MODULE_QUANTIZERS[Embedding]
 
-    pytorch_model = torch.quantization.quantize_dynamic(
+    if not skip_linear:
+        quantize_types[Linear] = MODULE_QUANTIZERS[Linear]
+
+    quantized_model = torch.quantization.quantize_dynamic(
         pytorch_model,
         quantize_types,
         dtype=torch.qint8,
     )
 
-    model.shims[0]._model = pytorch_model
+    if max_mse_loss:
+        quantized_model = requantize_with_max_loss(
+            pytorch_model, quantized_model, max_mse_loss
+        )
+
+    model.shims[0]._model = quantized_model
+
+
+def requantize_with_max_loss(
+    model: Module, quantized_model: Module, max_mse_loss: float
+):
+    loss = MSELoss()
+
+    modules = {name: child for name, child in model.named_modules()}
+    skipped_modules = []
+    module_qconfig = {}
+    for name, qmodule in quantized_model.named_modules():
+        if not (isinstance(qmodule, nnq.Embedding) or isinstance(qmodule, nnq.Linear)):
+            continue
+
+        module = modules[name]
+        mse_loss = loss(torch.dequantize(qmodule.weight()), module.weight)
+        if mse_loss <= max_mse_loss:
+            module_qconfig[name] = MODULE_QUANTIZERS[type(module)]
+        else:
+            skipped_modules.append(name)
+
+    print(f"Skipping modules with MSE > {max_mse_loss}: {', '.join(skipped_modules)}")
+
+    return torch.quantization.quantize_dynamic(
+        model,
+        module_qconfig,
+        dtype=torch.qint8,
+    )
 
 
 def pytorch_model(model: Model) -> Module:

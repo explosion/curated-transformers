0d93860ff39d37da1c297920cf811cb1896d4367
shademe <shadeMe@users.noreply.github.com>
2022-11-03 14:06:41 +0100

    Add `gradual_transformer_unfreezing` callback

--- curated_transformers/util.py
+++ curated_transformers/util.py
@@ -1,6 +1,11 @@
-from typing import Any, List, Set, Iterable
+from typing import Any, Callable, Any, List, Set, Iterable, Dict, TYPE_CHECKING
+from functools import partial
+from spacy.language import Language
 import itertools
 import thinc
+
+if TYPE_CHECKING:
+    from .pipe import Transformer
 
 thinc.registry.create("model_loaders", entry_points=True)
 registry = thinc.registry
@@ -43,3 +48,121 @@ def all_equal(iterable: Iterable[Any]) -> bool:
     (or if the input is an empty sequence), False otherwise."""
     g = itertools.groupby(iterable)
     return next(g, True) and not next(g, False)  # type: ignore
+
+
+def gradual_transformer_unfreezing_per_pipe(
+    nlp: Language,
+    callback_args: Dict[str, Any],
+    freeze_params: Dict[str, int],
+):
+    current_step = callback_args["step"]
+
+    # Scoped import to avoid import cycles.
+    from .pipe import Transformer
+
+    for name, pipe in nlp.components:
+        unfreeze_step = freeze_params.get(name)
+        if unfreeze_step is None:
+            continue
+        elif not isinstance(pipe, Transformer):
+            raise TypeError(
+                "Gradual unfreezing cannot be performed on non-Transformer component '{name}'"
+            )
+
+        pipe.frozen = current_step < unfreeze_step
+
+
+def gradual_transformer_unfreezing_all_pipes(
+    nlp: Language, callback_args: Dict[str, Any], unfreeze_step: int
+):
+    current_step = callback_args["step"]
+
+    # Scoped import to avoid import cycles.
+    from .pipe import Transformer
+
+    for _, pipe in nlp.components:
+        if not isinstance(pipe, Transformer):
+            continue
+
+        pipe.frozen = current_step < unfreeze_step
+
+
+def create_gradual_transformer_unfreezing(
+    target_pipes: Dict[str, int]
+) -> Callable[[Language, Dict[str, Any]], None]:
+    unfreeze_step_all_pipes = target_pipes.get("*")
+    if unfreeze_step_all_pipes is not None and len(target_pipes) > 1:
+        raise ValueError(
+            "Wildcard operator cannot be used in conjunction with individual pipe names"
+        )
+
+    if unfreeze_step_all_pipes is not None:
+        return partial(
+            gradual_transformer_unfreezing_all_pipes,
+            unfreeze_step=unfreeze_step_all_pipes,
+        )
+    else:
+        return partial(
+            gradual_transformer_unfreezing_per_pipe,
+            freeze_params=target_pipes,
+            last_unfreeze_step=max(target_pipes.values()),
+        )
+
+
+def gradual_transformer_unfreezing_per_pipe(
+    nlp: Language,
+    callback_args: Dict[str, Any],
+    freeze_params: Dict[str, int],
+):
+    current_step = callback_args["step"]
+
+    # Scoped import to avoid import cycles.
+    from .pipe import Transformer
+
+    for name, pipe in nlp.components:
+        unfreeze_step = freeze_params.get(name)
+        if unfreeze_step is None:
+            continue
+        elif not isinstance(pipe, Transformer):
+            raise TypeError(
+                "Gradual unfreezing cannot be performed on non-Transformer component '{name}'"
+            )
+
+        pipe.frozen = current_step < unfreeze_step
+
+
+def gradual_transformer_unfreezing_all_pipes(
+    nlp: Language, callback_args: Dict[str, Any], unfreeze_step: int
+):
+    current_step = callback_args["step"]
+
+    # Scoped import to avoid import cycles.
+    from .pipe import Transformer
+
+    for _, pipe in nlp.components:
+        if not isinstance(pipe, Transformer):
+            continue
+
+        pipe.frozen = current_step < unfreeze_step
+
+
+def create_gradual_transformer_unfreezing(
+    target_pipes: Dict[str, int]
+) -> Callable[[Language, Dict[str, Any]], None]:
+    unfreeze_step_all_pipes = target_pipes.get("*")
+    if unfreeze_step_all_pipes is not None and len(target_pipes) > 1:
+        raise ValueError(
+            "Wildcard operator cannot be used in conjunction with individual pipe names"
+        )
+
+    if unfreeze_step_all_pipes is not None:
+        return partial(
+            gradual_transformer_unfreezing_all_pipes,
+            unfreeze_step=unfreeze_step_all_pipes,
+        )
+    else:
+        return partial(
+            gradual_transformer_unfreezing_per_pipe,
+            freeze_params=target_pipes,
+            last_unfreeze_step=max(target_pipes.values()),
+        )

c6bd69e73097c2f4c8413498cff970d7ef6a6275
shademe <shadeMe@users.noreply.github.com>
2023-02-06 19:07:24 +0100

    Rename `listener` to `listeners`

--- curated_transformers/pipeline/transformer.py
+++ curated_transformers/pipeline/transformer.py
@@ -21,7 +21,7 @@ from thinc.model import Model
 from thinc.types import Ragged
 
 from ..models.output import DocTransformerOutput, TransformerModelOutput
-from ..models.listener import TransformerListener
+from ..models.listeners import TransformerListener
 
 DEFAULT_CONFIG_STR = """
     [transformer]

20eea2c338a6d05d0272288f8c953c08731cffc7
Madeesh Kannan <shadeMe@users.noreply.github.com>
2023-02-06 10:25:04 +0100

    Batch the spans that are passed to the torch transformer model (#85)
    * Batch the spans that are passed to the torch transformer model
    
    This places an upper-bound on the actual batch size of the transformer inputs. Previously, a single, very long document could yield a very large batch of inputs, potentially resulting in a OOM situation.
    
    * Fix assertion
    
    * Split test
    
    * `Transformer`: Remove `max_batch_items` in favour of batched spans
    
    * Fix indentation
    
    * `mypy` fix
    
    * Remove unused import

--- curated_transformers/pipe.py
+++ curated_transformers/pipe.py
@@ -22,11 +22,9 @@ from thinc.types import Ragged
 
 from .models.output import DocTransformerOutput, TransformerModelOutput
 from .listener import TransformerListener
-from .util import batch_by_length
 
 DEFAULT_CONFIG_STR = """
     [transformer]
-    max_batch_items = 4096
 
     [transformer.model]
     @architectures = "curated-transformers.XLMRTransformer.v1"
@@ -50,7 +48,6 @@ def make_transformer(
     name: str,
     model: Model,
     *,
-    max_batch_items: int = 4096,
     frozen: bool = False,
     all_layer_outputs: bool = False,
 ) -> "Transformer":
@@ -78,7 +75,6 @@ def make_transformer(
         name=name,
         frozen=frozen,
         all_layer_outputs=all_layer_outputs,
-        max_batch_items=max_batch_items,
     )
 
 
@@ -100,7 +96,6 @@ class Transformer(TrainablePipe):
         name: str = "transformer",
         frozen: bool = False,
         all_layer_outputs: bool = False,
-        max_batch_items: int = 4096,
     ) -> None:
         """
         vocab (Vocab):
@@ -120,7 +115,7 @@ class Transformer(TrainablePipe):
         self.model = model
         self.name = name
         self.listener_map: Dict[str, List[TransformerListener]] = {}
-        self.cfg = {"max_batch_items": max_batch_items}
+        self.cfg: Dict[str, Any] = {}
 
         _install_extensions()
         self.frozen = frozen
@@ -197,12 +192,10 @@ class Transformer(TrainablePipe):
             Processed documents in order.
         """
         _install_extensions()
-        for outer_batch in minibatch(stream, batch_size):
-            outer_batch = list(outer_batch)
-            for indices in batch_by_length(outer_batch, self.cfg["max_batch_items"]):
-                subbatch = [outer_batch[i] for i in indices]
-                self.set_annotations(subbatch, self.predict(subbatch))
-            yield from outer_batch
+        for batch in minibatch(stream, batch_size):
+            preds = self.predict(batch)
+            self.set_annotations(batch, preds)
+            yield from batch
 
     def predict(self, docs: Iterable[Doc]) -> TransformerModelOutput:
         """Apply the pipeline's model to a batch of docs, without modifying them.

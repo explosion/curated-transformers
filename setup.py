284b80ddc36b04e1c8871cf8b7a235b35edb191d
Daniël de Kok <me@danieldk.eu>
2023-02-06 13:08:50 +0100

    Bump version to 0.0.2
    Also expose the version through a Python module like Thinc/spaCy.

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+from pathlib import Path
 from setuptools import setup, Extension, find_packages
 from distutils.command.build_ext import build_ext
+
+
+root = Path(__file__).parent
+
+with (root / "curated_transformers" / "about.py").open("r") as f:
+    about = {}
+    exec(f.read(), about)
 
 
 def setup_package():
     setup(
         name="curated-transformers",
+        version=about["__version__"],
         packages=find_packages(),
     )
 

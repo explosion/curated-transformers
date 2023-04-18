9808fa0ed390502df978152e09a76780bd0c1896
Daniël de Kok <me@danieldk.eu>
2023-04-18 11:17:12 +0200

    Restore TorchScript functionality (necessary for quantization) (#129)
    * Add TorchScript and deepcopy tests
    
    * TorchScript fix: ModuleList can only be indexed with literals
    
    * TorchScript fix: **kwargs is not allowed
    
    * TorchScript fix: we cannot condition on global state
    
    So remove our custom workaround for macOS 13.2, this is fixed
    in 13.3.
    
    * TorchScript fix: TorchScript does not allow Module type annotation
    
    * TorchScript fixes: many fixes for the Attention class
    
    - Ensure TorchScript type inference works.
    - We can't reference global variables, including errors.
    - Dataclasses do not work well.
    - We need __init__ that can be found in source (not synthesized).
    - The tuple type only works fully specified (not Tuple or Tuple[int, ...])
    
    * Revert "Add support for Torch `scaled_dot_product_attention` (#128)"
    
    This reverts commit 68a355af7706094ded587a6895e70e70d727133b.
    
    The functionality introduced in this PR uses global state to detect
    whether the `scaled_dot_product_attention` is available and check
    whether the user want to use it. However, we cannot rely on global
    state in TorchScript.
    
    * Attempt to fix CI pip issues
    
    * Describe some TorchScript rules of thumb in DEVELOP.md
    
    * Simplify TorchScript type inference
    
    * Remove unused imports

--- curated_transformers/models/pytorch/albert/layer_group.py
+++ curated_transformers/models/pytorch/albert/layer_group.py
@@ -1,6 +1,7 @@
 from torch import Tensor
 from torch.nn import Module, ModuleList
 
+from ..attention import AttentionMask
 from ..bert.config import BertAttentionConfig
 from ..bert.layer import BertAttentionConfig, BertEncoderLayer
 from .config import AlbertLayerConfig
@@ -19,12 +20,12 @@ class AlbertLayerGroup(Module):
             ]
         )
 
-    def forward(self, input: Tensor, **kwargs) -> Tensor:
+    def forward(self, input: Tensor, attn_mask: AttentionMask) -> Tensor:
         """
         Shapes:
             input - (batch, seq_len, width)
         """
         layer_output = input
         for layer in self.group_layers:
-            layer_output = layer(layer_output, **kwargs)
+            layer_output = layer(layer_output, attn_mask)
         return layer_output

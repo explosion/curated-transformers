90950434eff536729b8649a8207e15ec50a5f58a
Daniël de Kok <me@danieldk.eu>
2023-05-09 17:17:45 +0200

    Add GPT-NeoX tokenizer (#141)
    * Add GPT-NeoX tokenizer
    
    Just off-the-shelf Byte BPE.
    
    * test_roberta -> test_roberta_tokenizer for consistency
    
    * Doc fix.
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>

---
+++ curated_transformers/tests/tokenization/test_gpt_neox_tokenizer.py
@@ -0,0 +1,234 @@
+import pytest
+import torch
+
+from curated_transformers._compat import has_hf_transformers, transformers
+from curated_transformers.tokenization import GPTNeoXTokenizer, PiecesWithIds
+
+from ..util import torch_assertclose
+
+
+@pytest.fixture
+def toy_tokenizer(test_dir):
+    return GPTNeoXTokenizer.from_files(
+        vocab_path=test_dir / "toy-vocab.json", merges_path=test_dir / "toy-merges.txt"
+    )
+
+
+@pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
+@pytest.mark.slow
+def test_gptneox_tokenizer_against_hf_tokenizer(sample_texts):
+    tokenizer = GPTNeoXTokenizer.from_hf_hub(name="EleutherAI/gpt-neox-20b")
+    pieces = tokenizer(sample_texts)
+
+    hf_tokenizer = transformers.AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+    hf_pieces = hf_tokenizer(sample_texts)
+
+    assert pieces.ids == hf_pieces["input_ids"]
+
+
+def test_gpt_neox_toy_tokenizer(toy_tokenizer, short_sample_texts):
+    encoding = toy_tokenizer(short_sample_texts)
+    _check_toy_tokenizer(encoding)
+
+
+def _check_toy_tokenizer(pieces):
+    assert isinstance(pieces, PiecesWithIds)
+    assert len(pieces.ids) == 2
+    assert len(pieces.pieces) == 2
+
+    assert pieces.lens == [
+        [1, 1, 1, 3, 1, 1, 7],
+        [3, 1, 1, 2, 4, 4, 3, 1, 2],
+    ]
+    assert pieces.ids == [
+        [
+            44,
+            997,
+            262,
+            305,
+            334,
+            79,
+            342,
+            262,
+            388,
+            79,
+            302,
+            70,
+            472,
+            72,
+            17,
+        ],
+        [
+            55,
+            841,
+            321,
+            362,
+            579,
+            324,
+            294,
+            291,
+            494,
+            131,
+            106,
+            270,
+            307,
+            79,
+            15,
+            298,
+            303,
+            86,
+            287,
+            317,
+            4,
+        ],
+    ]
+    assert pieces.pieces == [
+        [
+            "I",
+            "Ġsaw",
+            "Ġa",
+            "Ġg",
+            "ir",
+            "l",
+            "Ġwith",
+            "Ġa",
+            "Ġte",
+            "l",
+            "es",
+            "c",
+            "op",
+            "e",
+            ".",
+        ],
+        [
+            "T",
+            "od",
+            "ay",
+            "Ġwe",
+            "Ġwill",
+            "Ġe",
+            "at",
+            "Ġp",
+            "ok",
+            "Ã",
+            "©",
+            "Ġb",
+            "ow",
+            "l",
+            ",",
+            "Ġl",
+            "ot",
+            "s",
+            "Ġof",
+            "Ġit",
+            "!",
+        ],
+    ]
+
+    torch_assertclose(
+        pieces.padded_tensor(padding_id=1),
+        torch.tensor(
+            [
+                [
+                    44,
+                    997,
+                    262,
+                    305,
+                    334,
+                    79,
+                    342,
+                    262,
+                    388,
+                    79,
+                    302,
+                    70,
+                    472,
+                    72,
+                    17,
+                    1,
+                    1,
+                    1,
+                    1,
+                    1,
+                    1,
+                ],
+                [
+                    55,
+                    841,
+                    321,
+                    362,
+                    579,
+                    324,
+                    294,
+                    291,
+                    494,
+                    131,
+                    106,
+                    270,
+                    307,
+                    79,
+                    15,
+                    298,
+                    303,
+                    86,
+                    287,
+                    317,
+                    4,
+                ],
+            ],
+            dtype=torch.int32,
+        ),
+    )
+    torch_assertclose(
+        pieces.attention_mask,
+        torch.tensor(
+            [
+                [
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    False,
+                    False,
+                    False,
+                    False,
+                    False,
+                    False,
+                ],
+                [
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                    True,
+                ],
+            ]
+        ),
+    )
---
+++ curated_transformers/tokenization/gpt_neox_tokenizer.py
@@ -0,0 +1,44 @@
+from typing import Any, Type, TypeVar
+from cutlery import ByteBPEProcessor
+import json
+from pathlib import Path
+
+from .bbpe_tokenizer import ByteBPETokenizer
+
+# Only provided as typing.Self in Python 3.11+.
+Self = TypeVar("Self", bound="GPTNeoXTokenizer")
+
+
+class GPTNeoXTokenizer(ByteBPETokenizer):
+    """GPT-NeoX tokenizer (Black et al., 2022).
+
+    The GPT-NeoX tokenizer uses byte-level byte pair encoding.
+    """
+
+    @classmethod
+    def from_files(
+        cls: Type[Self],
+        *,
+        vocab_path: Path,
+        merges_path: Path,
+    ) -> Self:
+        """Construct a tokenizer from the vocabulary and merges files.
+
+        vocab_path (Path): path to the vocabulary file.
+        merges_path (Path): path to the merges file.
+        """
+        processor = ByteBPEProcessor.load_from_files(
+            vocab=vocab_path, merges=merges_path
+        )
+        return cls(processor=processor)
+
+    @classmethod
+    def _convert_hf_tokenizer(cls: Type[Self], tokenizer: Any) -> Self:
+        serialized = tokenizer.backend_tokenizer.to_str(True)  # type: ignore
+        deserialized = json.loads(serialized)
+        vocab_merges = deserialized["model"]
+        merges = [tuple(merge.split(" ")) for merge in vocab_merges["merges"]]
+        processor = ByteBPEProcessor(vocab_merges["vocab"], merges)
+        return cls(
+            processor=processor,
+        )

7f4a651ea9db747aabdda4cdcf6f8faca9f3535c
Daniël de Kok <me@danieldk.eu>
2023-05-15 12:27:20 +0200

    Move piece id retrieval to the RobertaTokenizer constructor
    Let the pre/post-processors take the ids as argument.

--- curated_transformers/tokenization/roberta_tokenizer.py
+++ curated_transformers/tokenization/roberta_tokenizer.py
@@ -16,33 +16,23 @@ class RobertaPreDecoder(PreDecoder):
         self,
         *,
         processor: ByteBPEProcessor,
-        bos_piece: str = "<s>",
-        eos_piece: str = "</s>",
+        bos_id: int,
+        eos_id: int,
     ):
         """Construct a RoBERTa pre-decoder.
 
         processor (ByteBPEProcessor): The processor to wrap.
-        bos_piece (str): The piece to use to mark the beginning of a sequence.
-        eos_piece (str): The piece to use to mark the end of a sequence.
+        bos_id (int): The piece id used to mark the beginning of a sequence.
+        eos_id (int): The piece id used to mark the end of a sequence.
         """
-        self.bos_piece = bos_piece
-        self.eos_piece = eos_piece
+        self.bos_id = bos_id
+        self.eos_id = eos_id
         self.processor = processor
 
     def __call__(self, input: Iterable[Iterable[int]]) -> List[List[int]]:
-        bos_id = self.processor.piece_id(self.bos_piece)
-        if bos_id is None:
-            raise ValueError(
-                f"RoBERTa piece encoder vocabulary doesn't contain '{self.bos_piece}' piece"
-            )
-
-        eos_id = self.processor.piece_id(self.eos_piece)
-        if eos_id is None:
-            raise ValueError(
-                f"RoBERTa piece encoder vocabulary doesn't contain '{self.eos_piece}' piece"
-            )
-
-        return [[id for id in ids if id not in (bos_id, eos_id)] for ids in input]
+        return [
+            [id for id in ids if id not in (self.bos_id, self.eos_id)] for ids in input
+        ]
 
 
 class RobertaPostEncoder(PostEncoder):
@@ -50,35 +40,29 @@ class RobertaPostEncoder(PostEncoder):
         self,
         *,
         processor: ByteBPEProcessor,
-        bos_piece: str = "<s>",
-        eos_piece: str = "</s>",
+        bos_piece: str,
+        eos_piece: str,
+        bos_id: int,
+        eos_id: int,
     ):
         """Construct a RoBERTa post-encoder.
 
         processor (ByteBPEProcessor): The processor to wrap.
-        bos_piece (str): The piece to use to mark the beginning of a sequence.
-        eos_piece (str): The piece to use to mark the end of a sequence.
+        bos_piece (str): The piece used to mark the beginning of a sequence.
+        eos_piece (str): The piece used to mark the end of a sequence.
+        bos_id (int): The piece id used to mark the beginning of a sequence.
+        eos_id (int): The piece id used to mark the end of a sequence.
         """
         self.bos_piece = bos_piece
         self.eos_piece = eos_piece
+        self.bos_id = bos_id
+        self.eos_id = eos_id
         self.processor = processor
 
     def __call__(self, pieces_with_ids: PiecesWithIds) -> PiecesWithIds:
-        bos_id = self.processor.piece_id(self.bos_piece)
-        if bos_id is None:
-            raise ValueError(
-                f"RoBERTa piece encoder vocabulary doesn't contain '{self.bos_piece}' piece"
-            )
-
-        eos_id = self.processor.piece_id(self.eos_piece)
-        if eos_id is None:
-            raise ValueError(
-                f"RoBERTa piece encoder vocabulary doesn't contain '{self.eos_piece}' piece"
-            )
-
         ids = []
         for seq_ids in pieces_with_ids.ids:
-            ids.append([bos_id] + seq_ids + [eos_id])
+            ids.append([self.bos_id] + seq_ids + [self.eos_id])
         pieces = []
         for seq_pieces in pieces_with_ids.pieces:
             pieces.append([self.bos_piece] + seq_pieces + [self.eos_piece])
@@ -107,13 +91,18 @@ class RobertaTokenizer(ByteBPETokenizer):
 
         self.processor = processor
 
+        bos_id = _get_piece_id_or_fail(processor, bos_piece)
+        eos_id = _get_piece_id_or_fail(processor, eos_piece)
+
         self.pre_decoder = RobertaPreDecoder(
-            bos_piece=bos_piece, eos_piece=eos_piece, processor=processor
+            bos_id=bos_id, eos_id=eos_id, processor=processor
         )
 
         self.post_encoder = RobertaPostEncoder(
             bos_piece=bos_piece,
             eos_piece=eos_piece,
+            bos_id=bos_id,
+            eos_id=eos_id,
             processor=processor,
         )
 
@@ -156,3 +145,12 @@ class RobertaTokenizer(ByteBPETokenizer):
             bos_piece=bos_piece,
             eos_piece=eos_piece,
         )
+
+
+def _get_piece_id_or_fail(processor: ByteBPEProcessor, piece: str):
+    piece_id = processor.piece_id(piece)
+    if piece_id is None:
+        raise ValueError(
+            f"RoBERTa piece encoder vocabulary doesn't contain '{piece}' piece"
+        )
+    return piece_id

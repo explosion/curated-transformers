8e882a24e87f0716cc22bda5693401fd5b6f445f
shadeMe <shadeMe@users.noreply.github.com>
2023-05-16 14:26:47 +0200

    Add unit test

---
+++ curated_transformers/tests/models/test_hf_hub.py
@@ -0,0 +1,15 @@
+import pytest
+
+from curated_transformers._compat import has_hf_transformers
+from curated_transformers.models.bert.encoder import BertEncoder
+
+from ..conftest import TORCH_DEVICES
+from .util import assert_encoder_output_equals_hf
+
+
+@pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
+@pytest.mark.parametrize("torch_device", TORCH_DEVICES)
+def test_sharded_model_checkpoints(torch_device):
+    assert_encoder_output_equals_hf(
+        BertEncoder, "explosion-testing/bert-test-sharded", torch_device
+    )

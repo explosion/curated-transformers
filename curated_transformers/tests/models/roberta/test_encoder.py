91d02c9b3bf074f612c1bc0177bf82f37328b541
Daniël de Kok <me@danieldk.eu>
2023-05-16 16:27:01 +0200

    Update encoder tests to use small models from `explosion-testing` (#162)
    Also drop all the `slow` annotations.

--- curated_transformers/tests/models/roberta/test_encoder.py
+++ curated_transformers/tests/models/roberta/test_encoder.py
@@ -9,6 +9,7 @@ from ..util import assert_encoder_output_equals_hf
 
 @pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
 @pytest.mark.parametrize("torch_device", TORCH_DEVICES)
-@pytest.mark.slow
 def test_encoder(torch_device):
-    assert_encoder_output_equals_hf(RobertaEncoder, "roberta-base", torch_device)
+    assert_encoder_output_equals_hf(
+        RobertaEncoder, "explosion-testing/roberta-test", torch_device
+    )

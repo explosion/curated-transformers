acc7202bcba3c23a397ec5a498cd97cf469a127a
shadeMe <shadeMe@users.noreply.github.com>
2023-05-23 15:53:02 +0200

    Sort imports

--- curated_transformers/tokenization/__init__.py
+++ curated_transformers/tokenization/__init__.py
@@ -1,9 +1,9 @@
-from .tokenizer import PiecesWithIds, Tokenizer
 from .bbpe_tokenizer import ByteBPETokenizer
-from .wordpiece_tokenizer import WordPieceTokenizer
+from .bert_tokenizer import BertTokenizer
+from .camembert_tokenizer import CamembertTokenizer
 from .gpt_neox_tokenizer import GPTNeoXTokenizer
 from .roberta_tokenizer import RobertaTokenizer
-from .bert_tokenizer import BertTokenizer
 from .sentencepiece_tokenizer import SentencePieceTokenizer
+from .tokenizer import PiecesWithIds, Tokenizer
+from .wordpiece_tokenizer import WordPieceTokenizer
 from .xlmr_tokenizer import XlmrTokenizer
-from .camembert_tokenizer import CamembertTokenizer

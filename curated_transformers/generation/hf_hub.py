8968e53c052ad5869f53e5e845629e0224d2460e
Daniël de Kok <me@danieldk.eu>
2023-06-02 17:07:55 +0200

    Don't forget the `hf_hub` module

---
+++ curated_transformers/generation/hf_hub.py
@@ -0,0 +1,21 @@
+from typing import Optional, Type, TypeVar
+from abc import ABC, abstractmethod
+import torch
+
+
+# Only provided as typing.Self in Python 3.11+.
+Self = TypeVar("Self", bound="FromHFHub")
+
+
+class FromHFHub(ABC):
+    @classmethod
+    @abstractmethod
+    def from_hf_hub(
+        cls: Type[Self],
+        *,
+        name: str,
+        revision: str = "main",
+        device: Optional[torch.device] = None
+    ) -> Self:
+        """Load a generator from Huggingface Hub."""
+        ...

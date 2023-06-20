178ecb14e201c5b830b4adf11d121827c8933471
shadeMe <shadeMe@users.noreply.github.com>
2023-06-20 14:57:52 +0200

    Move more HF loading code to `util.hf`

--- curated_transformers/models/hf_hub.py
+++ curated_transformers/models/hf_hub.py
@@ -3,17 +3,11 @@ from abc import ABC, abstractmethod
 from typing import Any, Iterable, Mapping, Optional, Type, TypeVar
 
 import torch
-from huggingface_hub import hf_hub_download
-from requests import HTTPError  # type: ignore
 from torch import Tensor
 
 from ..quantization import BitsAndBytesConfig, prepare_module_for_quantization
+from ..util.hf import get_model_checkpoint_filepaths, get_model_config_filepath
 from ..util.serde import load_model_from_checkpoints
-
-HF_MODEL_CONFIG = "config.json"
-HF_MODEL_CHECKPOINT = "pytorch_model.bin"
-HF_MODEL_SHARDED_CHECKPOINT_INDEX = "pytorch_model.bin.index.json"
-HF_MODEL_SHARDED_CHECKPOINT_INDEX_WEIGHTS_KEY = "weight_map"
 
 # Only provided as typing.Self in Python 3.11+.
 Self = TypeVar("Self", bound="FromPretrainedHFModel")
@@ -85,7 +79,7 @@ class FromPretrainedHFModel(ABC):
             Module with the parameters loaded.
         """
         # Download configuration and construct model.
-        config_filename = _get_model_config_filepath(name, revision)
+        config_filename = get_model_config_filepath(name, revision)
         with open(config_filename, "r") as f:
             config = json.load(f)
         # Initialize the model on the torch `meta` device to avoid unnecessary allocations.
@@ -106,7 +100,7 @@ class FromPretrainedHFModel(ABC):
             tensor2param = None
 
         # Download model and convert HF parameter names to ours.
-        checkpoint_filenames = _get_model_checkpoint_filepaths(name, revision)
+        checkpoint_filenames = get_model_checkpoint_filepaths(name, revision)
         load_model_from_checkpoints(
             model,  # type:ignore
             filepaths=checkpoint_filenames,
@@ -136,59 +130,3 @@ class FromPretrainedHFModel(ABC):
         order to be an abstract base class.
         """
         ...
-
-
-def _get_model_config_filepath(name: str, revision: str) -> str:
-    try:
-        return hf_hub_download(
-            repo_id=name, filename=HF_MODEL_CONFIG, revision=revision
-        )
-    except:
-        raise ValueError(
-            f"Couldn't find a valid config for model `{name}` "
-            f"(revision `{revision}`) on HuggingFace Model Hub"
-        )
-
-
-def _get_model_checkpoint_filepaths(name: str, revision: str) -> Iterable[str]:
-    # Attempt to download a non-sharded checkpoint first.
-    try:
-        model_filename = hf_hub_download(
-            repo_id=name, filename=HF_MODEL_CHECKPOINT, revision=revision
-        )
-    except HTTPError:
-        # We'll get a 404 HTTP error for sharded models.
-        model_filename = None
-
-    if model_filename is not None:
-        return [model_filename]
-
-    try:
-        model_index_filename = hf_hub_download(
-            repo_id=name, filename=HF_MODEL_SHARDED_CHECKPOINT_INDEX, revision=revision
-        )
-    except HTTPError:
-        raise ValueError(
-            f"Couldn't find a valid PyTorch checkpoint for model `{name}` "
-            f"(revision `{revision}`) on HuggingFace Model Hub"
-        )
-
-    with open(model_index_filename, "r") as f:
-        index = json.load(f)
-
-    weight_map = index.get(HF_MODEL_SHARDED_CHECKPOINT_INDEX_WEIGHTS_KEY)
-    if weight_map is None or not isinstance(weight_map, dict):
-        raise ValueError(
-            f"Invalid index file in sharded PyTorch checkpoint for model `{name}`"
-        )
-
-    filepaths = []
-    # We shouldn't need to hold on to the weights map in the index as each checkpoint
-    # should contain its constituent parameter names.
-    for filename in set(weight_map.values()):
-        resolved_filename = hf_hub_download(
-            repo_id=name, filename=filename, revision=revision
-        )
-        filepaths.append(resolved_filename)
-
-    return sorted(filepaths)

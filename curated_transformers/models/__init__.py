9d94b696705c2461e25be9b5f5447f1ac9714456
shadeMe <shadeMe@users.noreply.github.com>
2023-06-20 14:58:54 +0200

    Export missing encoders in `models`

--- curated_transformers/models/__init__.py
+++ curated_transformers/models/__init__.py
@@ -2,5 +2,8 @@ from .activations import GeluNew
 from .albert.encoder import AlbertEncoder
 from .attention import AttentionMask
 from .bert.encoder import BertEncoder
+from .camembert.encoder import CamembertEncoder
 from .gpt_neox import GPTNeoXCausalLM, GPTNeoXDecoder
+from .refined_web_model import RefinedWebModelCausalLM, RefinedWebModelDecoder
 from .roberta.encoder import RobertaEncoder
+from .xlm_roberta import XlmRobertaEncoder

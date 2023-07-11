742a6a39a5fa6ebe1b77965fe187e22f28640f3b
Daniël de Kok <me@danieldk.eu>
2023-07-11 14:34:53 +0200

    Add an example that rewrites sentences in the passive voice to active voice (#224)
    * Add an example that rewrites sentences in the passive voice to active voice
    
    We use `FalconGenerator` rather than `AutoGenerator` because the prompt
    was created with Falcon 7B with instruction tuning.
    
    * Make RefinedWebModel quantizable
    
    * passive2active: add --quantize flag

--- curated_transformers/models/refined_web_model/causal_lm.py
+++ curated_transformers/models/refined_web_model/causal_lm.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Mapping, Optional, Type, TypeVar
+from typing import Any, List, Mapping, Optional, Set, Type, TypeVar
 
 import torch
 from torch import Tensor
@@ -6,6 +6,7 @@ from torch.nn import Linear
 
 from ...layers.attention import AttentionMask
 from ...layers.cache import KeyValueCache
+from ...quantization.quantizable import Quantizable
 from ..hf_hub import FromHFHub
 from ..module import CausalLMModule
 from ..output import CausalLMOutputWithCache
@@ -17,7 +18,7 @@ from .decoder import RefinedWebModelDecoder
 Self = TypeVar("Self", bound="RefinedWebModelCausalLM")
 
 
-class RefinedWebModelCausalLM(CausalLMModule[KeyValueCache], FromHFHub):
+class RefinedWebModelCausalLM(CausalLMModule[KeyValueCache], FromHFHub, Quantizable):
     """
     Refined Web Model (eg. Falcon) causal language model.
     """
@@ -72,3 +73,8 @@ class RefinedWebModelCausalLM(CausalLMModule[KeyValueCache], FromHFHub):
     ) -> Self:
         config = convert_hf_config(hf_config)
         return cls(config, device=device)
+
+    @classmethod
+    def modules_to_not_quantize(cls) -> Set[str]:
+        # Ignore the output embedding matrix.
+        return {"output_embeddings"}

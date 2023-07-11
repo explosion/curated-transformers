8972e5d4043eb3a947a84a06f7363ddbcf7be71c
shadeMe <shadeMe@users.noreply.github.com>
2023-07-11 17:40:40 +0200

    Add checking for missing `scipy` dependency when importing `bnb`

--- curated_transformers/_compat.py
+++ curated_transformers/_compat.py
@@ -1,3 +1,5 @@
+import warnings
+
 try:
     import transformers
 
@@ -6,10 +8,43 @@ except ImportError:
     transformers = None  # type: ignore
     has_hf_transformers = False
 
-try:
-    import bitsandbytes
 
-    has_bitsandbytes = True
-except ImportError:
-    bitsandbytes = None  # type: ignore
-    has_bitsandbytes = False
+def _check_scipy_presence() -> bool:
+    try:
+        from scipy.stats import norm
+
+        return True
+    except ImportError:
+        return False
+
+
+def _check_bnb_presence() -> bool:
+    try:
+        import bitsandbytes
+    except ModuleNotFoundError:
+        return False
+    except ImportError:
+        pass
+    return True
+
+
+bitsandbytes = None  # type: ignore
+has_bitsandbytes = False
+
+# As of v0.40.0, bitsandbytes doesn't correctly specify `scipy` as an installation
+# dependency. This can lead to situations where the former is installed but
+# the latter isn't and the ImportError gets masked. So, we additionally check
+# for the presence of `scipy`.
+if _check_scipy_presence():
+    try:
+        del bitsandbytes
+        import bitsandbytes  # type: ignore
+
+        has_bitsandbytes = True
+    except ImportError:
+        bitsandbytes = None  # type: ignore
+elif _check_bnb_presence():
+    warnings.warn(
+        "The `bitsandbytes` library is installed but its dependency "
+        "`scipy` isn't. Please install `scipy` to correctly load `bitsandbytes`."
+    )

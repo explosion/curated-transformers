9f7a665d9ddac11ab63a5345ea1dbe8007ab879b
Madeesh Kannan <shadeMe@users.noreply.github.com>
2023-07-12 09:03:45 +0200

    Remove shim for `bitsandbytes` versions that don't support the `device` parameter (#228)
    * Remove shim for `bitsandbytes` versions that don't support the device parameter
    
    * Add checking for missing `scipy` dependency when importing `bnb`
    
    * `mypy` fix
    
    * Also set variables when bitsandbytes is not installed
    
    ---------
    
    Co-authored-by: Daniël de Kok <me@danieldk.eu>

--- curated_transformers/_compat.py
+++ curated_transformers/_compat.py
@@ -1,3 +1,5 @@
+import warnings
+
 try:
     import transformers
 
@@ -6,19 +8,43 @@ except ImportError:
     transformers = None  # type: ignore
     has_hf_transformers = False
 
-try:
-    import bitsandbytes
 
-    has_bitsandbytes = True
-
-    # Check if we have the correct version (that exposes the `device` parameter).
+def _check_scipy_presence() -> bool:
     try:
-        _ = bitsandbytes.nn.Linear8bitLt(1, 1, device=None)
-        _ = bitsandbytes.nn.Linear4bit(1, 1, device=None)
-        has_bitsandbytes_linear_device = True
-    except TypeError:
-        has_bitsandbytes_linear_device = False
-except ImportError:
+        from scipy.stats import norm
+
+        return True
+    except ImportError:
+        return False
+
+
+def _check_bnb_presence() -> bool:
+    try:
+        import bitsandbytes
+    except ModuleNotFoundError:
+        return False
+    except ImportError:
+        pass
+    return True
+
+
+# As of v0.40.0, `bitsandbytes` doesn't correctly specify `scipy` as an installation
+# dependency. This can lead to situations where the former is installed but
+# the latter isn't and the ImportError gets masked. So, we additionally check
+# for the presence of `scipy`.
+if _check_scipy_presence():
+    try:
+        import bitsandbytes
+
+        has_bitsandbytes = True
+    except ImportError:
+        bitsandbytes = None  # type: ignore
+        has_bitsandbytes = False
+else:
+    if _check_bnb_presence():
+        warnings.warn(
+            "The `bitsandbytes` library is installed but its dependency "
+            "`scipy` isn't. Please install `scipy` to correctly load `bitsandbytes`."
+        )
     bitsandbytes = None  # type: ignore
     has_bitsandbytes = False
-    has_bitsandbytes_linear_device = False

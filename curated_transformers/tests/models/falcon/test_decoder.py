49f3d825f053145ae07ca5e1a556c5f10c42f315
Daniël de Kok <me@danieldk.eu>
2023-07-14 17:59:04 +0200

    Support Falcon with non-parallel attention and no multi-query (#244)
    * Support Falcon with non-parallel attention and no multi-query
    
    - Add non-parallel attention to the decoding layer.
    - Use `QkvMode.MERGED_SPLIT_BEFORE` when multi-query is disabled.
    
    * Update falcon-no-parallel-attn-test revision

--- curated_transformers/tests/models/falcon/test_decoder.py
+++ curated_transformers/tests/models/falcon/test_decoder.py
@@ -26,26 +26,37 @@ VOCAB_SIZE = 1024
 
 
 FALCON_TEST_MODELS = [
-    "explosion-testing/falcon-test",
-    "explosion-testing/refined-web-model-test",
+    (
+        "explosion-testing/falcon-no-parallel-attn-test",
+        "f9122de6cafad10159af214786fa11b89cc37a89",
+    ),
+    ("explosion-testing/falcon-test", "24ff3d5fd83b4d174888356f20e61349f6cbf467"),
+    (
+        "explosion-testing/refined-web-model-test",
+        "57a7a9829a4b6fce833152c4c20a46c7056f9cc1",
+    ),
 ]
 
 
 @pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
 @pytest.mark.parametrize("torch_device", TORCH_DEVICES)
-@pytest.mark.parametrize("model", FALCON_TEST_MODELS)
-def test_decoder(torch_device, model):
+@pytest.mark.parametrize("model_revision", FALCON_TEST_MODELS)
+def test_decoder(torch_device, model_revision):
+    model, revision = model_revision
+
     hf_model = transformers.AutoModel.from_pretrained(
-        "explosion-testing/falcon-test",
+        model,
         # Safe because it is under our control.
         trust_remote_code=True,
         # Avoid warnings about trusting remote code without a revision.
-        revision="235f4b64c489e33ae7e40163ab1f266bbe355651",
+        revision=revision,
     )
     hf_model.to(torch_device)
     hf_model.eval()
 
-    model = FalconDecoder.from_hf_hub(name=model, device=torch_device)
+    model = FalconDecoder.from_hf_hub(
+        name=model, revision=revision, device=torch_device
+    )
     model.eval()
 
     torch.manual_seed(0)
@@ -60,9 +71,13 @@ def test_decoder(torch_device, model):
 
 @pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
 @pytest.mark.parametrize("torch_device", TORCH_DEVICES)
-@pytest.mark.parametrize("model", FALCON_TEST_MODELS)
-def test_decoder_with_cache(torch_device, model):
-    model = FalconDecoder.from_hf_hub(name=model, device=torch_device)
+@pytest.mark.parametrize("model_revision", FALCON_TEST_MODELS)
+def test_decoder_with_cache(torch_device, model_revision):
+    model, revision = model_revision
+
+    model = FalconDecoder.from_hf_hub(
+        name=model, revision=revision, device=torch_device
+    )
     model.eval()
 
     torch.manual_seed(0)

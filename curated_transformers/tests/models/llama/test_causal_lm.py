2abc7a6df9d8ce3f0f7d06f3334fd3ea5d3189f9
Daniël de Kok <me@danieldk.eu>
2023-07-26 17:59:55 +0200

    Add support for LLaMA2 (#263)
    * Add support for LLaMA2
    
    LLaMA2 support different number of query and key-value heads. Also add
    new models to test k-n and k-1 head configurations.
    
    * Remove transformers pin

--- curated_transformers/tests/models/llama/test_causal_lm.py
+++ curated_transformers/tests/models/llama/test_causal_lm.py
@@ -6,14 +6,21 @@ from ...compat import has_hf_transformers, has_torch_compile
 from ...conftest import TORCH_DEVICES
 from ..util import JITMethod, assert_causal_lm_output_equals_hf
 
+LLAMA_TEST_MODELS = [
+    "trl-internal-testing/tiny-random-LlamaForCausalLM",
+    "explosion-testing/llama2-fewer-kv-heads",
+    "explosion-testing/llama2-kv-sharing",
+]
+
 
 @pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
 @pytest.mark.parametrize("torch_device", TORCH_DEVICES)
 @pytest.mark.parametrize("with_torch_sdp", [False, True])
-def test_causal_lm(torch_device, with_torch_sdp):
+@pytest.mark.parametrize("model", LLAMA_TEST_MODELS)
+def test_causal_lm(torch_device, model, with_torch_sdp):
     assert_causal_lm_output_equals_hf(
         LLaMACausalLM,
-        "trl-internal-testing/tiny-random-LlamaForCausalLM",
+        model,
         torch_device,
         with_torch_sdp=with_torch_sdp,
     )
@@ -23,11 +30,12 @@ def test_causal_lm(torch_device, with_torch_sdp):
 @pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
 @pytest.mark.skipif(not has_torch_compile, reason="requires torch.compile")
 @pytest.mark.parametrize("torch_device", TORCH_DEVICES)
+@pytest.mark.parametrize("model", LLAMA_TEST_MODELS)
 @pytest.mark.parametrize("with_torch_sdp", [False, True])
-def test_causal_lm_with_torch_compile(torch_device, with_torch_sdp):
+def test_causal_lm_torch_compile(torch_device, model, with_torch_sdp):
     assert_causal_lm_output_equals_hf(
         LLaMACausalLM,
-        "trl-internal-testing/tiny-random-LlamaForCausalLM",
+        model,
         torch_device,
         jit_method=JITMethod.TorchCompile,
         with_torch_sdp=with_torch_sdp,
@@ -37,11 +45,12 @@ def test_causal_lm_with_torch_compile(torch_device, with_torch_sdp):
 @pytest.mark.slow
 @pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
 @pytest.mark.parametrize("torch_device", TORCH_DEVICES)
+@pytest.mark.parametrize("model", LLAMA_TEST_MODELS)
 @pytest.mark.parametrize("with_torch_sdp", [False, True])
-def test_causal_lm_with_torchscript_trace(torch_device, with_torch_sdp):
+def test_causal_lm_with_torchscript_trace(torch_device, model, with_torch_sdp):
     assert_causal_lm_output_equals_hf(
         LLaMACausalLM,
-        "trl-internal-testing/tiny-random-LlamaForCausalLM",
+        model,
         torch_device,
         jit_method=JITMethod.TorchScriptTrace,
         with_torch_sdp=with_torch_sdp,
--- curated_transformers/tests/models/llama/test_decoder.py
+++ curated_transformers/tests/models/llama/test_decoder.py
@@ -6,16 +6,20 @@ from ...compat import has_hf_transformers, has_torch_compile
 from ...conftest import TORCH_DEVICES
 from ..util import JITMethod, assert_decoder_output_equals_hf
 
+LLAMA_TEST_MODELS = [
+    "trl-internal-testing/tiny-random-LlamaForCausalLM",
+    "explosion-testing/llama2-fewer-kv-heads",
+    "explosion-testing/llama2-kv-sharing",
+]
+
 
 @pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
 @pytest.mark.parametrize("torch_device", TORCH_DEVICES)
+@pytest.mark.parametrize("model", LLAMA_TEST_MODELS)
 @pytest.mark.parametrize("with_torch_sdp", [False, True])
-def test_decoder(torch_device, with_torch_sdp):
+def test_decoder(torch_device, model, with_torch_sdp):
     assert_decoder_output_equals_hf(
-        LLaMADecoder,
-        "trl-internal-testing/tiny-random-LlamaForCausalLM",
-        torch_device,
-        with_torch_sdp=with_torch_sdp,
+        LLaMADecoder, model, torch_device, with_torch_sdp=with_torch_sdp
     )
 
 
@@ -23,11 +27,12 @@ def test_decoder(torch_device, with_torch_sdp):
 @pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
 @pytest.mark.skipif(not has_torch_compile, reason="requires torch.compile")
 @pytest.mark.parametrize("torch_device", TORCH_DEVICES)
+@pytest.mark.parametrize("model", LLAMA_TEST_MODELS)
 @pytest.mark.parametrize("with_torch_sdp", [False, True])
-def test_decoder_with_torch_compile(torch_device, with_torch_sdp):
+def test_decoder_with_torch_compile(torch_device, model, with_torch_sdp):
     assert_decoder_output_equals_hf(
         LLaMADecoder,
-        "trl-internal-testing/tiny-random-LlamaForCausalLM",
+        model,
         torch_device,
         jit_method=JITMethod.TorchCompile,
         with_torch_sdp=with_torch_sdp,
@@ -37,11 +42,12 @@ def test_decoder_with_torch_compile(torch_device, with_torch_sdp):
 @pytest.mark.slow
 @pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
 @pytest.mark.parametrize("torch_device", TORCH_DEVICES)
+@pytest.mark.parametrize("model", LLAMA_TEST_MODELS)
 @pytest.mark.parametrize("with_torch_sdp", [False, True])
-def test_decoder_with_torchscript_trace(torch_device, with_torch_sdp):
+def test_decoder_with_torchscript_trace(torch_device, model, with_torch_sdp):
     assert_decoder_output_equals_hf(
         LLaMADecoder,
-        "trl-internal-testing/tiny-random-LlamaForCausalLM",
+        model,
         torch_device,
         jit_method=JITMethod.TorchScriptTrace,
         with_torch_sdp=with_torch_sdp,

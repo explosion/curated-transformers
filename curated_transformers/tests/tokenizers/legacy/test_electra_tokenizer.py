2d070f0173d40a3f05723629aaf84a184100801c
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-01-03 12:19:06 +0100

    formatted using black

--- curated_transformers/tests/tokenizers/legacy/test_electra_tokenizer.py
+++ curated_transformers/tests/tokenizers/legacy/test_electra_tokenizer.py
@@ -10,9 +10,12 @@ from ..util import compare_tokenizer_outputs_with_hf_tokenizer
 
 @pytest.mark.skipif(not has_hf_transformers, reason="requires huggingface transformers")
 @pytest.mark.parametrize(
-    "model_name", ["jonfd/electra-small-nordic", "Maltehb/aelaectra-danish-electra-small-cased", "google/electra-small-discriminator"]
+    "model_name",
+    [
+        "jonfd/electra-small-nordic",
+        "Maltehb/aelaectra-danish-electra-small-cased",
+        "google/electra-small-discriminator",
+    ],
 )
 def test_from_hf_hub_equals_hf_tokenizer(model_name: str, sample_texts):
-    compare_tokenizer_outputs_with_hf_tokenizer(
-        sample_texts, model_name, BERTTokenizer
-    )
+    compare_tokenizer_outputs_with_hf_tokenizer(sample_texts, model_name, BERTTokenizer)
